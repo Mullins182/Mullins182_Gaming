7ca062eedf1469dd69080ec06c704f578839f5db
Mullins182 <Mullins182@t-online.de>
2025-05-14 12:11:49 +0200

    moved player collision logic, playerOnLift and playerCanLeave( ) to playerLogic.mjs

--- elevators_from_hell/js/hell10.mjs
+++ elevators_from_hell/js/hell10.mjs
@@ -10,7 +10,7 @@ import {
   player_spriteSheet,
 } from "./spriteHandling.mjs";
 
-import { playerMovandColl, isColliding } from "./playerLogic.mjs";
+import { playerMovandColl, isColliding, playerOnLift } from "./playerLogic.mjs";
 
 import {
   npcButtonPress,
@@ -928,92 +928,6 @@ function exitDoor() {
   }
 }
 
-// ___________________________ PLAYER CAN LEAVE BUILDING-CHECK ___________________________
-
-export function playerCanLeave() {
-  return flexElemsPosInit.exitDoorPosY < gameCanvas.height * 0.73 &&
-    flexElemsPosInit.playerPosX < 500 &&
-    flexElemsPosInit.playerOnFloor == 0
-    ? true
-    : false;
-}
-
-// ___________________________ PLAYER ON LIFT-CHECK ___________________________
-
-function playerOnLift(getOutOfLift) {
-  if (getOutOfLift) {
-    if (flexElemsPosInit.playerOnLiftL) {
-      if (!flexElemsPosInit.liftL_isMoving && shaftLdoorsOpenCheck()) {
-        flexElemsPosInit.playerOnLiftL = false;
-      }
-    }
-    if (flexElemsPosInit.playerOnLiftR) {
-      if (!flexElemsPosInit.liftR_isMoving && shaftRdoorsOpenCheck()) {
-        flexElemsPosInit.playerOnLiftR = false;
-      }
-    }
-  } else {
-    if (
-      (flexElemsPosInit.playerOnFloor === 0 &&
-        flexElemsPosInit.liftL_isOnFloor === 0) ||
-      (flexElemsPosInit.playerOnFloor === 1 &&
-        flexElemsPosInit.liftL_isOnFloor === 1) ||
-      (flexElemsPosInit.playerOnFloor === 2 &&
-        flexElemsPosInit.liftL_isOnFloor === 2) ||
-      (flexElemsPosInit.playerOnFloor === 3 &&
-        flexElemsPosInit.liftL_isOnFloor === 3) ||
-      (flexElemsPosInit.playerOnFloor === 4 &&
-        flexElemsPosInit.liftL_isOnFloor === 4) ||
-      (flexElemsPosInit.playerOnFloor === 5 &&
-        flexElemsPosInit.liftL_isOnFloor === 5) ||
-      (flexElemsPosInit.playerOnFloor === 6 &&
-        flexElemsPosInit.liftL_isOnFloor === 6)
-    ) {
-      if (
-        flexElemsPosInit.playerPosX > gameCanvas.width * 0.16 &&
-        flexElemsPosInit.playerPosX < gameCanvas.width * 0.185
-      ) {
-        flexElemsPosInit.playerOnLiftL = true;
-      }
-    }
-    if (
-      (flexElemsPosInit.playerOnFloor === 0 &&
-        flexElemsPosInit.liftR_isOnFloor === 0) ||
-      (flexElemsPosInit.playerOnFloor === 1 &&
-        flexElemsPosInit.liftR_isOnFloor === 1) ||
-      (flexElemsPosInit.playerOnFloor === 2 &&
-        flexElemsPosInit.liftR_isOnFloor === 2) ||
-      (flexElemsPosInit.playerOnFloor === 3 &&
-        flexElemsPosInit.liftR_isOnFloor === 3) ||
-      (flexElemsPosInit.playerOnFloor === 4 &&
-        flexElemsPosInit.liftR_isOnFloor === 4) ||
-      (flexElemsPosInit.playerOnFloor === 5 &&
-        flexElemsPosInit.liftR_isOnFloor === 5) ||
-      (flexElemsPosInit.playerOnFloor === 6 &&
-        flexElemsPosInit.liftR_isOnFloor === 6)
-    ) {
-      if (
-        flexElemsPosInit.playerPosX > gameCanvas.width * 0.76 &&
-        flexElemsPosInit.playerPosX < gameCanvas.width * 0.79
-      ) {
-        flexElemsPosInit.playerOnLiftR = true;
-      }
-    }
-  }
-}
-
-export function playerCollision() {
-  return flexElemsPosInit.playerPosX >=
-    gameCanvas.width * 0.95 - gameElements.playerWidth / 1.85
-    ? true
-    : flexElemsPosInit.playerPosX <=
-      gameCanvas.width * 0.05 +
-        gameElements.wallsWidth -
-        gameElements.playerWidth / 2.25
-    ? true
-    : false;
-}
-
 // ___________________________ SHAFT-DOORS-LOGIC ___________________________
 
 function shaftDoors() {
@@ -1538,7 +1452,7 @@ function shaftRdoorsClosed() {
   return Object.values(shaftRdoorsClosedStatus).every(Boolean);
 }
 
-function shaftRdoorsOpenCheck() {
+export function shaftRdoorsOpenCheck() {
   return Object.values(shaftRdoorsOpenStatus).some(Boolean);
 }
 
@@ -1546,7 +1460,7 @@ function shaftLdoorsClosed() {
   return Object.values(shaftLdoorsClosedStatus).every(Boolean);
 }
 
-function shaftLdoorsOpenCheck() {
+export function shaftLdoorsOpenCheck() {
   return Object.values(shaftLdoorsOpenStatus).some(Boolean);
 }
 
--- elevators_from_hell/js/playerLogic.mjs
+++ elevators_from_hell/js/playerLogic.mjs
@@ -1,6 +1,5 @@
 import {
-  playerCollision,
-  playerCanLeave,
+  gameCanvas,
   playerPosUpdate,
   gameElements,
   currentFramePlayer,
@@ -8,6 +7,8 @@ import {
   flexElemsPosInit,
   animationIntervalPlayer,
   lastTimePlayer,
+  shaftLdoorsOpenCheck,
+  shaftRdoorsOpenCheck,
 } from "./hell10.mjs";
 import { changePlayerSprite } from "./spriteHandling.mjs";
 
@@ -32,4 +33,86 @@ export function playerMovandColl() {
     animationIntervalPlayer = 125; // Reset des Intervalls
     lastTimePlayer = performance.now(); // Reset des Zeitstempels
   }
+} // ___________________________ PLAYER CAN LEAVE BUILDING-CHECK ___________________________
+
+export function playerCanLeave() {
+  return flexElemsPosInit.exitDoorPosY < gameCanvas.height * 0.73 &&
+    flexElemsPosInit.playerPosX < 500 &&
+    flexElemsPosInit.playerOnFloor == 0
+    ? true
+    : false;
+}
+// ___________________________ PLAYER ON LIFT-CHECK ___________________________
+export function playerOnLift(getOutOfLift) {
+  if (getOutOfLift) {
+    if (flexElemsPosInit.playerOnLiftL) {
+      if (!flexElemsPosInit.liftL_isMoving && shaftLdoorsOpenCheck()) {
+        flexElemsPosInit.playerOnLiftL = false;
+      }
+    }
+    if (flexElemsPosInit.playerOnLiftR) {
+      if (!flexElemsPosInit.liftR_isMoving && shaftRdoorsOpenCheck()) {
+        flexElemsPosInit.playerOnLiftR = false;
+      }
+    }
+  } else {
+    if (
+      (flexElemsPosInit.playerOnFloor === 0 &&
+        flexElemsPosInit.liftL_isOnFloor === 0) ||
+      (flexElemsPosInit.playerOnFloor === 1 &&
+        flexElemsPosInit.liftL_isOnFloor === 1) ||
+      (flexElemsPosInit.playerOnFloor === 2 &&
+        flexElemsPosInit.liftL_isOnFloor === 2) ||
+      (flexElemsPosInit.playerOnFloor === 3 &&
+        flexElemsPosInit.liftL_isOnFloor === 3) ||
+      (flexElemsPosInit.playerOnFloor === 4 &&
+        flexElemsPosInit.liftL_isOnFloor === 4) ||
+      (flexElemsPosInit.playerOnFloor === 5 &&
+        flexElemsPosInit.liftL_isOnFloor === 5) ||
+      (flexElemsPosInit.playerOnFloor === 6 &&
+        flexElemsPosInit.liftL_isOnFloor === 6)
+    ) {
+      if (
+        flexElemsPosInit.playerPosX > gameCanvas.width * 0.16 &&
+        flexElemsPosInit.playerPosX < gameCanvas.width * 0.185
+      ) {
+        flexElemsPosInit.playerOnLiftL = true;
+      }
+    }
+    if (
+      (flexElemsPosInit.playerOnFloor === 0 &&
+        flexElemsPosInit.liftR_isOnFloor === 0) ||
+      (flexElemsPosInit.playerOnFloor === 1 &&
+        flexElemsPosInit.liftR_isOnFloor === 1) ||
+      (flexElemsPosInit.playerOnFloor === 2 &&
+        flexElemsPosInit.liftR_isOnFloor === 2) ||
+      (flexElemsPosInit.playerOnFloor === 3 &&
+        flexElemsPosInit.liftR_isOnFloor === 3) ||
+      (flexElemsPosInit.playerOnFloor === 4 &&
+        flexElemsPosInit.liftR_isOnFloor === 4) ||
+      (flexElemsPosInit.playerOnFloor === 5 &&
+        flexElemsPosInit.liftR_isOnFloor === 5) ||
+      (flexElemsPosInit.playerOnFloor === 6 &&
+        flexElemsPosInit.liftR_isOnFloor === 6)
+    ) {
+      if (
+        flexElemsPosInit.playerPosX > gameCanvas.width * 0.76 &&
+        flexElemsPosInit.playerPosX < gameCanvas.width * 0.79
+      ) {
+        flexElemsPosInit.playerOnLiftR = true;
+      }
+    }
+  }
+}
+
+export function playerCollision() {
+  return flexElemsPosInit.playerPosX >=
+    gameCanvas.width * 0.95 - gameElements.playerWidth / 1.85
+    ? true
+    : flexElemsPosInit.playerPosX <=
+      gameCanvas.width * 0.05 +
+        gameElements.wallsWidth -
+        gameElements.playerWidth / 2.25
+    ? true
+    : false;
 }
