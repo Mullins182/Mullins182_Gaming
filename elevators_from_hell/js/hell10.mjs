2157ccd9dd5e1e075b8f46024c23c66799ab3ad0
Mullins182 <Mullins182@t-online.de>
2026-04-03 01:34:57 +0200

    Update game version to v1.4.5

--- elevators_from_hell/js/hell10.mjs
+++ elevators_from_hell/js/hell10.mjs
@@ -62,7 +62,7 @@ export const homeButton = document.getElementById("homeButton");
 let soundsLoaded = false;
 
 // ___________________________ GAME-VERSION ___________________________
-export let gameVersion = "v1.4.3";
+export let gameVersion = "v1.4.5";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM INITIALIZED !");
